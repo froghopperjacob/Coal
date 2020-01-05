18345f3828845cf730b680e7a6c2a2d8e85bd9c5
Jacob <froghopperjacob@gmail.com>
2020-01-05 14:45:47 -0500

    Update Main.lua

--- src/Modules/Main.lua
+++ src/Modules/Main.lua
@@ -4,7 +4,46 @@ return function()
 	local Lexer, Parser, Validator, Interpreter = import("Lexer", "Parser", "Validator", "Interpreter")
 	
 	Class.Main = function(self, options)
-		self.options = options or {}
+		local defScope = {
+			["import"] = function(scope, ...)
+				local toImport = { ... }
+				
+				for i = 1, #toImport do					
+					local ret = import("Imports." .. toImport[i])
+										
+					scope[ret["className"]] = ret
+				end
+			end	
+		}
+		
+		local defSendScope = {
+			
+		}
+		
+		self.options = options or {
+			["Scope"] = defScope,
+			["SendScope"] = defSendScope
+		}
+			
+		if (self.options["Scope"] ~= defScope) then
+			local nScope = defScope
+			
+			for i, v in pairs(self.options["Scope"]) do
+				nScope[i] = v
+			end
+			
+			self.options["Scope"] = nScope
+		end
+		
+		if (self.options["SendScope"] ~= defSendScope) then
+			local nSScope = defSendScope
+			
+			for i, v in pairs(self.options["SendScope"]) do
+				nSScope[i] = v
+			end
+			
+			self.options["SendScope"] = nSScope
+		end
 		
 		return self
 	end
@@ -14,7 +53,7 @@ return function()
 			ParserTimeStart, ParserTimeEnd,
 			ValidatorTimeStart, ValidatorTimeEnd,
 			InterpretTimeStart, InterpreterTimeEnd,
-			ret, globalScope, scopes = nil, nil, nil, nil, nil, nil, 0, 0, nil, self.options["Scope"] or {}, nil
+			ret, globalScope, sendScope, scopes = nil, nil, nil, nil, nil, nil, 0, 0, nil, self.options["Scope"], self.options["SendScope"], nil
 		
 		if (not self.options) then
 			return error('Class not initalized')
@@ -41,8 +80,8 @@ return function()
 		ValidatorTimeEnd = tick()
 		
 		if (valid) then
-			InterpretTimeStart = tick()
-			globalScope, scopes = Interpreter:interpret(AST, tokens, globalScope)
+			InterpretTimeStart = tick()			
+			globalScope, scopes = Interpreter:interpret(AST, tokens, globalScope, sendScope)
 			InterpreterTimeEnd = tick()
 		end
 		
